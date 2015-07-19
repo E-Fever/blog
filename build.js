77ee1ead488e2825f89f2f4a78c1b75ebad68560
Steven Mak <stevenmak@gmail.com>
2015-07-20 00:20:22 +0800

    A very basic blogging system using Metalsmith

--- build.js
+++ build.js
@@ -1,4 +1,13 @@
-var metalsmith = require('metalsmith');
+var metalsmith = require('metalsmith'),
+    markdown = require('metalsmith-markdown'),
+    templates = require('metalsmith-templates'),
+    serve = require('metalsmith-serve'),
+    watch = require('metalsmith-watch'),
+    excerpts = require('metalsmith-excerpts'),
+    collections = require('metalsmith-collections'),
+    branch = require('metalsmith-branch'),
+    permalinks = require('metalsmith-permalinks'),
+    moment = require('moment');
 
 var siteBuild = metalsmith(__dirname)
     .metadata({
@@ -9,6 +18,38 @@ var siteBuild = metalsmith(__dirname)
     })
     .source('./src')
     .destination('./build')
+    .use(markdown())
+    .use(excerpts())
+    .use(collections({
+      posts: {
+        pattern: 'posts/**.html',
+        sortBy: 'publishDate',
+        reverse: true
+      }
+    }))
+    .use(branch('posts/**.html')
+        .use(permalinks({
+          pattern: 'posts/:title',
+          relative: false
+        }))
+    )
+    .use(branch('!posts/**.html')
+        .use(branch('!index.md').use(permalinks({
+          relative: false
+        })))
+    )
+    .use(templates({
+      engine: 'jade',
+      moment: moment
+    }))
+    .use(serve({
+      port: 8080,
+      verbose: true
+    }))
+    .use(watch({
+      pattern: '**/*',
+      livereload: true
+    }))
     .build(function (err) {
       if (err) {
         console.log(err);
---
+++ src/posts/test_post.md
@@ -0,0 +1,8 @@
+---
+title: test post
+publishDate: 2015-07-19
+author: Steven Mak
+template: post.jade
+---
+
+This is a test post
