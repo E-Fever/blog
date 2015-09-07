4c46801ab44ec318f92bafccaa042e0f4ca03f8c
Steven Mak <stevenmak@gmail.com>
2015-09-07 16:44:59 +0800

    Need more information to see why build doesn't work

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ set -e # exit with nonzero exit code if anything fails
 rm -rf github_build || exit 0;
 git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" github_build;
 
+if [ -d ./build ];  then echo "./build"; fi
+if [ -d ./e-fever/blog/build ];  then echo ./"e-fever/blog/build"; fi
+
 cp -rf e-fever/blog/build/* github_build
 
 # go to the out directory and create a *new* Git repo
