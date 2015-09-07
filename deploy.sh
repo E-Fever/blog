e983d191e89c704a0560f404b9b3fcd54035fe30
Steven Mak <stevenmak@gmail.com>
2015-09-07 14:58:26 +0800

    Deploy to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ git commit -a --allow-empty-message -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io" master
+git push "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io" master
