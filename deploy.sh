b26b43489f37942ef1ff48532e36dd1a3a4bc70c
Steven Mak <stevenmak@gmail.com>
2015-09-07 16:58:51 +0800

    Another blind test for travis

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-rm -rf github_build || exit 0;
-git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" github_build;
+#rm -rf github_build || exit 0;
+#git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" github_build;
+
+rm -rf build || exit 0;
+git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" build;
+
+node ./build.js
 
 #cp -rf ./build/* github_build
-cp -rf /home/travis/build/e-fever/blog/build github_build
+#cp -rf /home/travis/build/e-fever/blog/build github_build
 
 # go to the out directory and create a *new* Git repo
 
@@ -15,8 +20,9 @@ cp -rf /home/travis/build/e-fever/blog/build github_build
 # files present with the commit message "Deploy to GitHub Pages".
 
 
+#cd github_build; \
 
-cd github_build; \
+cd build; \
   git add -A; \
   git commit -a --allow-empty-message -m "Deploy to GitHub Pages"; \
   git push "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io" master
