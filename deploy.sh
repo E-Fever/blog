951d0d6fbf544a2d14aace35f4919d9fa8439690
Steven Mak <stevenmak@gmail.com>
2015-09-07 17:23:21 +0800

    Another deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-#rm -rf github_build || exit 0;
-#git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" github_build;
+rm -rf github_build || exit 0;
+git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" github_build;
 
-rm -rf build || exit 0;
-git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" build;
+#rm -rf build || exit 0;
+#git clone "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io.git" build;
 
 node ./build.js
 
-#cp -rf ./build/* github_build
+cp -rf ./build/* github_build
 #cp -rf /home/travis/build/e-fever/blog/build github_build
 
 # go to the out directory and create a *new* Git repo
@@ -22,5 +22,5 @@ node ./build.js
 
 #cd github_build; \
 
-cd build; git config --global user.name "Steven Mak"; git config --global user.email "stevenmak@gmail.com"; git add -A; git commit -a -m "Deploy to GitHub Pages";  git push "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io" master
+cd github_build; git config user.name "Steven Mak"; git config user.email "stevenmak@gmail.com"; git add -A; git commit -a -m "Deploy to GitHub Pages";  git push "https://${GH_TOKEN}@github.com/e-fever/e-fever.github.io" master
 
